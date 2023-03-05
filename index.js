ed9a7897918b5be9f30e43ad04f7dd2085574fe9
Changhyun <chang.bae@flatironschool.com>
2023-03-05 04:14:50 -0800

    Array Filter Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(name,match){
+    const result = name.filter(name=>name===match||name===match.toLowerCase());
+    return result
+}
+function fuzzyMatch(name,match){
+    const result = name.filter(name=>name.charAt(0)===match.charAt(0));
+    return result
+}
+function matchName(drivers,match){
+    const result = drivers.filter(drivers=>drivers.name===match)
+        return(result)
+    }
